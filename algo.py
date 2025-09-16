1cc69230719daf976863228ffedab45c4e3cdc0e
KuroEinzbern <Alejandro.Damian.Bastida@hotmail.com>
2025-09-16 17:58:54 -0300

    al

--- algo.py
+++ algo.py
@@ -10,56 +10,56 @@ df = pd.read_csv("train.csv")
 # going crazy
 pd.set_option('display.max_rows', None)      
 
-# setting sex as binary
-df["Sex"] = df['Sex'].transform(lambda x: (x == "male").astype(int))
-
-# filling ages
-org_ages = df.loc[df['Age'].isna()].index
-df['Age'] = df['Age'].fillna(df.groupby(['Pclass', 'Sex'])['Age'].transform('median')).round()
-predicted_ages = df.loc[org_ages]
-
-# total family number
-df['FamilyNumber'] = df['SibSp'] + df['Parch']
-
-# cast cabin to binary
-df["Cabin"] = df['Cabin'].transform(lambda x: (~x.isna()).astype(bool))
-
-#first approac to filling embarked
-df['Embarked'] = df['Embarked'].fillna('S')
-
-#creation of ticketCount, farePerPerson and their transform to log scale
-df['TicketCount'] = df['Ticket'].transform(lambda x: len(df[df['Ticket'] == x]))
-df['FarePerPerson'] = df['Fare'] / df['TicketCount']
-df['FarePerPerson'] = np.where(df['FarePerPerson'] == 0, 0, np.log(df['FarePerPerson']))
-df['Fare'] = np.where(df['Fare'] == 0, 0, np.log(df['Fare']))
-
-#model titles in names
 def modelate_nobiliarie_titles(nombre):
     nobiliary_diccionary={'Mrs.':'Mrs','Miss.':'Miss','Mr.':'Mr','Master.':'Master'}
     match= re.search(r'\b\w+\.', nombre)
     nobiliary_title=match.group().lstrip()
     return nobiliary_diccionary.get(nobiliary_title,'Rare')
 
-#creating columns with titles
-df['Title']=df['Name'].map(modelate_nobiliarie_titles)
+def data_treatmeant(df):
+    df["Sex"] = df['Sex'].transform(lambda x: (x == "male").astype(int))
+    # filling ages
+    org_ages = df.loc[df['Age'].isna()].index
+    df['Age'] = df['Age'].fillna(df.groupby(['Pclass', 'Sex'])['Age'].transform('median')).round()
+    predicted_ages = df.loc[org_ages]
 
+    # total family number
+    df['FamilyNumber'] = df['SibSp'] + df['Parch']
 
+    # cast cabin to binary
+    df["Cabin"] = df['Cabin'].transform(lambda x: (~x.isna()).astype(bool))
 
-#drop unnecesarie and noise columns
-del df['Ticket']
-del df['PassengerId']
-del df['Name']
-del df['Fare']
+    #first approac to filling embarked
+    df['Embarked'] = df['Embarked'].fillna('S')
+
+    #creation of ticketCount, farePerPerson and their transform to log scale
+    df['TicketCount'] = df['Ticket'].transform(lambda x: len(df[df['Ticket'] == x]))
+    df['FarePerPerson'] = df['Fare'] / df['TicketCount']
+    df['FarePerPerson'] = np.where(df['FarePerPerson'] == 0, 0, np.log(df['FarePerPerson']))
+    df['Fare'] = np.where(df['Fare'] == 0, 0, np.log(df['Fare']))
+    df['Title']=df['Name'].map(modelate_nobiliarie_titles)
+    del df['Ticket']
+    del df['PassengerId']
+    del df['Name']
+    del df['Fare']
+    df = pd.get_dummies(df, columns=['Embarked',"Title"], prefix=['OheEmbarked','OheTitle'])
+    return df
+
 y=df.pop('Survived')
 
-df = pd.get_dummies(df, columns=['Embarked',"Title"], prefix=['OheEmbarked','OheTitle'])
-
 X_train, X_test, y_train, y_test = train_test_split(df, y, random_state=0,test_size=0.1)
+X_train_treated= data_treatmeant(X_train)
+x_test_treated=data_treatmeant(X_test)
 
 model= LogisticRegression(max_iter=10000)
-model.fit(X_train,y_train)
+model.fit(X_train_treated,y_train)
 print(model.score(X_train,y_train))
 print(model.score(X_test,y_test))
+
+#wat
+
+
+#model titles in names
 
 
 
